f6bc6691d5cef548a3a88622004a2c349158910d
JIAH LEE <66239292+JIAH-LEA@users.noreply.github.com>
2021-02-27 14:14:03 +0900

    구글 서비스 등록

--- src/main/java/com/leeahwants/book/springboot/service/PostsService.java
+++ src/main/java/com/leeahwants/book/springboot/service/PostsService.java
@@ -10,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Service
@@ -30,9 +33,23 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
